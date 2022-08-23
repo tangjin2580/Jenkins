8abc5a4003d6884ae27212534e61993667bf8efb
李鑫 <2324099478@qq.com>
2022-08-23 09:57:00 +0800

    分天保存构建历史

--- Jenkins/Jenkins_history.py
+++ Jenkins/Jenkins_history.py
@@ -4,7 +4,8 @@ import jenkinsapi
 import openpyxl
 import datetime
 
-now_time = datetime.datetime.now().strftime("%Y/%m/%d")
+now_time = datetime.datetime.now().strftime("%Y-%m-%d")
+
 print(now_time)
 
 
@@ -15,8 +16,8 @@ def get_build_history(jk) -> list:
         number = job.get_next_build_number()
         if number == 1:
             continue
-        for n in range(1, 1):
-        # for n in range(number - 2, number):
+        # for n in range(1, 1):
+        for n in range(number - 2, number):
             try:
                 b = job.get_build(n)
                 # branch1 = b._get_vcs()
@@ -32,8 +33,8 @@ def get_build_history(jk) -> list:
                 'number': n,
                 'node': b.get_slave() or 'master',
                 'branch': str(b._get_vcs),
-                'Datestamp': b.get_timestamp().strftime("%Y/%m/%d"),
-                'timestamp': b.get_timestamp().strftime("%Y/%m/%d-%H:%M:%S"),
+                'Datestamp': b.get_timestamp().strftime("%Y-%m-%d"),
+                'timestamp': b.get_timestamp().strftime("%Y%m-%d-%H:%M:%S"),
                 'duration': round(b.get_duration().total_seconds()),
                 'status': b.get_status(),
                 'cause': b.get_causes()[0].get('shortDescription').replace('Started by ', '')
@@ -58,10 +59,9 @@ add_file_path: str = 'jenkins\history'
 if not os.path.exists(add_file_path):
     print('文件夹', add_file_path, '不存在,已创建')
     os.makedirs(add_file_path)
-    file_path = os.getcwd()+os.sep+add_file_path
+    file_path = os.getcwd() + os.sep + add_file_path
 
- file_name= now_time+'Jenkins构建历史.xlsx'
-
-wb.save("./jenkins/history/Jenkins构建历史.xlsx")
+file_name: str = 'jenkins/history/' + now_time + '-Jenkins构建历史.xlsx'
+wb.save(file_name)
 
 wb.close()
